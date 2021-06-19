67dcf4d3337a9d8ec5fddc0ed5b6fb97ccbe0fc2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:58:06 +0200

    sm8150-common: Move excluded-input-devices.xml to vibrator folder
    Change-Id: I6f384c556e124052b3695a87f225207dae7c8089

--- msmnile.mk
+++ msmnile.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_msmnile
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
